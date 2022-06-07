e10393ea2b54da665d40c4f86c1afb09f14d1358
cricri777 <christian.bruneau@simpleetdonnees.com>
2022-06-06 20:55:32 -0400

    todo leetcode: Squares of a Sorted Array

---
+++ src/ca/cricri/exercices/learn/SquaresOfSortedArray.java
@@ -0,0 +1,13 @@
+package ca.cricri.exercices.learn;
+
+public class SquaresOfSortedArray {
+
+    /**
+     * Algorithme : parcourir array dans les 2 sens prendre la plus grande valeur absolue des 2 et continuer
+     * à itérer.
+     * TODO : <a href="https://leetcode.com/explore/learn/card/fun-with-arrays/521/introduction/3240/">link</a>
+     */
+    public int[] sortedSquares(int[] nums) {
+        return null;
+    }
+}
