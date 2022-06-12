95b5246d8ad81f062cece42a9dfbcf00401a3648
cricri777 <christian.bruneau@simpleetdonnees.com>
2022-06-12 17:31:58 -0400

    new Leetcode problems

---
+++ src/ca/cricri/problems/LeetCode75Questions.java
@@ -0,0 +1,29 @@
+package ca.cricri.problems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * TODO = <a href="https://leetcode.com/discuss/general-discussion/460599/blind-75-leetcode-questions">link</a>
+ * TODO = <a href="https://www.techinterviewhandbook.org/coding-interview-study-plan/">link</a>
+ */
+public class LeetCode75Questions {
+
+    /**
+     * 1st problem
+     * <a href="https://leetcode.com/problems/two-sum/">link</a>
+     */
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] res = new int[2];
+
+        for(int i=0; i<nums.length; i++) {
+            if(map.containsKey(target - nums[i])){
+                return new int[] {i, map.get(target - nums[i])};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return null;
+    }
+}
